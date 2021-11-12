e4150e4338c7e195c0c88089697261a54ba3fa6c
malykhinaIra <irinamalykhina21@gmail.com>
2021-11-12 18:51:21 +0200

    new

--- lab3/test.py
+++ lab3/test.py
@@ -12,7 +12,6 @@ result = 0
 for line in file.readlines():
     if line.strip().isdigit():
         result+=int(line.strip())
-file.close()
 """
 print(timeit.timeit(s, number=5))
 
@@ -22,7 +21,6 @@ result = 0
 for line in file:
     if line.strip().isdigit():
         result+=int(line.strip())
-file.close()
 """
 
 print(timeit.timeit(s, number=5))
@@ -30,6 +28,5 @@ print(timeit.timeit(s, number=5))
 s = """
 file = open("test.txt", "r")
 result = sum(int(line.strip()) for line in file if line.strip().isdigit())
-file.close()
 """
 print(timeit.timeit(s, number=5))
